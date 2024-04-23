624b2a7685b3b1e0440cd76d8515add405c6540b
koundinya21 <149105895+Koundinya21@users.noreply.github.com>
2024-04-24 01:26:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {Component} from 'react'
+// import {useState} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
 import './App.css'
-// import ScoreContext from './ReactContext/index'
+
+// import DetailsContext from './ReactContext'
 import LoginForm from './components/LoginForm'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './components/Home'
@@ -10,48 +11,17 @@ import Results from './components/Results'
 import NotFound from './components/NotFound'
 import TimeUp from './components/TimeUp'
 
-class App extends Component {
-  //   state = {score: 0}
-
-  //   updatingScore = () => {
-  //     this.setState(prevScore => ({
-  //       score: prevScore.score + 1,
-  //     }))
-  //   }
-
-  //   render() {
-  //     const {score} = this.state
-  //     return (
-  //       <ScoreContext.Provider value={{score, updatingScore: this.updatingScore}}>
-  //         <Switch>
-  //           <Route exact path="/login" component={LoginForm} />
-  //           <ProtectedRoute exact path="/" component={Home} />
-  //           <ProtectedRoute
-  //             exact
-  //             path="/assess/questions"
-  //             component={Assessment}
-  //           />
-  //           <ProtectedRoute exact path="/results" component={Results} />
-  //           <ProtectedRoute exact path="/time-up" component={TimeUp} />
-  //           <Route path="/bad-path" component={NotFound} />
-  //           <Redirect to="/bad-path" />
-  //         </Switch>
-  //       </ScoreContext.Provider>
-  //     )
-  //   }
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/login" component={LoginForm} />
-        <ProtectedRoute exact path="/" component={Home} />
-        <ProtectedRoute exact path="/assess/questions" component={Assessment} />
-        <ProtectedRoute exact path="/results" component={Results} />
-        <ProtectedRoute exact path="/time-up" component={TimeUp} />
-        <Route path="/bad-path" component={NotFound} />
-        <Redirect to="/bad-path" />
-      </Switch>
-    )
-  }
-}
+const App = () => (
+  
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/assessment" component={Assessment} />
+    <ProtectedRoute exact path="/results" component={Results} />
+    <ProtectedRoute exact path="/time-up" component={TimeUp} />
+    <Route path="/bad-path" component={NotFound} />
+    <Redirect to="/bad-path" />
+  </Switch>
+)
 
 export default App
